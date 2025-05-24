14213fa584f3851297c918b5a9b39092e594e674
dingrui <Bannirui@outlook.com>
2025-05-25 01:38:45 +0800

    配置读取

--- conf/__init__.py
+++ conf/__init__.py
@@ -2,9 +2,9 @@ from pathlib import Path
 
 from .config import ConfigLoader, ServerConfig
 
-config_path:str=Path(__file__).parent.parent/"conf.toml"
+config_path: str = Path(__file__).parent.parent/"conf.toml"
 assert config_path.exists(), f"项目配置文件不存在:{config_path}"
 assert config_path.is_file(), f"{config_path}不是一个文件"
 
 # 项目全局配置
-app_cfg=ConfigLoader(config_path).get()
+app_cfg = ConfigLoader(config_path).get()
